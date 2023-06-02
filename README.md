2eb83ac56057d03567e443d89e4d38edb57ee9ff
Leandro Motta Barros <lmb@stackedboxes.org>
2023-06-02 18:00:34 -0300

    Properly document the source files

--- README.md
+++ README.md
@@ -1,37 +1,26 @@
-# I2C Display Example
+# I2C Display on balena Example
 
-Using a display (based on the SSD1306 controller) on a balena device.
+This is a simple and extensively commented example on how to use an I2C
+peripheral connected to a Raspberry Pi 3 running on the
+[balena](https://www.balena.io) platform. (Disclaimer: I work for balena.)
 
-## Doubts
+The concepts shall be the same regardless of which I2C peripheral you want to
+use, but here I am using a little 128x64 pixels monochrome OLED display driven
+by an SSD1306 chip. It's similar to [this Monochrome 0.96" 128x64 OLED Graphic
+Display](https://www.adafruit.com/product/326) by Adafruit.
 
-**Why don't we need to run in privileged mode, nor any capabilities?** Maybe
-because when we "share" a device with the container we get full access to the
-device (and in this case at least, we don't need to do anything low-level that
-is not via the device)? See a fragment of `balena inspect` on the container:
-
-```json
-"Devices": [
-    {
-        "PathOnHost": "/dev/i2c-1",
-        "PathInContainer": "/dev/i2c-1",
-        "CgroupPermissions": "rwm"
-    }
-],
-```
-
-**Why don't we need to load the i2c-dev kernel module manually? Who loads it?**
-Apparently it is loaded during boot... perhaps by the kernel itself when it sees
-that there is an i2c controller available? Not sure that's how things work
-(feels there's some piece I am missing).
+Anyway, go check the code. It's well-documented.
 
 ## Further experiments (left as exercise to the reader!)
 
 ### I2C Baud Rate
 
-Set the config variable `BALENA_HOST_CONFIG_dtparam` to
-`i2c_arm=on,i2c_baudrate=1000000` to increase the speed of the I2C communication
-(and therefore make the display refresh more quickly).
+I was able to improve the refresh times of my display by increasing the I2C baud
+rate. In a balena world, you simply need to set the config variable
+`BALENA_HOST_CONFIG_dtparam` to `i2c_arm=on,i2c_baudrate=1000000`.
 
-Note that this overrides balena defaults, so we have to set `i2c_arm=on`
-manually. You'd also need to explicitly set any other values that are set by
-default (e.g., `audio=on` if you need to use audio).
+The magic is really done by `i2c_baudrate=1000000`, but you also need to add
+`i2c_arm=on` to ensure I2C is enabled. balena will enable I2C by default, but
+since we are manually changing the `dtparam`s, we have to explicitly set all the
+values that matter for us. (This would be the same for other parameters that
+balena sets by default, like `audio=on`).
--- src/main.py
+++ src/main.py
@@ -1,32 +1,62 @@
 import time
 import board
-import busio
 import adafruit_ssd1306
 from PIL import Image, ImageDraw, ImageFont
 
-i2c = busio.I2C(board.SCL, board.SDA)
+# Create the object representing our 128x64 OLED display. It is connected to the
+# Raspberry Pi using the default I2C pins, so we can simply use `board.I2C()` to
+# refer to them. I am explicitly passing the I2C address of my device (0x3c)
+# because I am not sure what is the default used by the Adafruit library.
+#
+# Telling which GPIO pins are used and what is the address of the display is the
+# closest we'll get to I2C. The actual work of using the I2C protocol to send
+# commands to the display is abstracted by the Adafruit library. Other
+# peripherals will typically (or hopefully!) have their own specific libraries.
+#
+# And I am using hardcoded values for things like display resolution because I
+# want to keep the example as simple as possible. The average real-world
+# application would benefit from some "best practices" of programming.
+oled = adafruit_ssd1306.SSD1306_I2C(128, 64, board.I2C(), addr=0x3c)
 
-oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3c)
+# Here we create an image the same size as our display. We'll make all drawing
+# to this image and then blit (draw) it at once to the display.
+#
+# That funny "1" is the way to tell that this is a monochrome (1 bit per pixel)
+# image.
+image = Image.new("1", (128, 64))
 
-# 1-bit image
-image = Image.new("1", (oled.width, oled.height))
+# This is an object that is capable of drawing things on the image we just
+# created.
 draw = ImageDraw.Draw(image)
+
+# And this is the font we'll used to write some text into the display.
 font = ImageFont.load_default()
 
+# Nothing like an infinite loop :-)
 while True:
+    # We first draw a rectangle filling the whole screen. The `fill=0` means
+    # that each pixel of the rectangle will have the value 0 (that is, black).
     draw.rectangle([(0,0), (127,63)], fill=0)
+
+    # Now write a message to the image, now using `fill=255`, i.e., white (or
+    # whatever color your display is -- mine is actually blue).
     draw.text((4,4), "Hello", fill=255, font=font)
+
+    # Copy the image data to the display, and update (`show()`) the display.
     oled.image(image)
     oled.show()
+
+    # Leave the display alone for 1.5s to allow the user to read the message.
     time.sleep(1.5)
 
-    # Subliminar message :-)
+    # Same as above, but without the sleep. Just a basic subliminal message :-P
     draw.rectangle([(0,0), (127,63)], fill=0)
     draw.text((60,32), "balena!", fill=255, font=font)
     oled.image(image)
     oled.show()
-    # time.sleep(0.1)
 
+    # And the second part of the proper "Hello World" message, now in with a
+    # black text on white background.
     draw.rectangle([(0,0), (127,63)], fill=255)
     draw.text((20,20), "World", fill=0, font=font)
     oled.image(image)
